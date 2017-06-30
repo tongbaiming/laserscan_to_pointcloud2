bad3693a4e518aeb68fce1329e400fb7bd68b158
tongbaiming <tongbaiming@126.com>
2017-06-30 08:16:53 +0800

    xx

---
+++ include/laserscan_to_pointcloud2/laserscan_to_pointcloud2_node.h
@@ -0,0 +1,18 @@
+/*
+ * laserscan_to_pointcloud2_node.h
+ *
+ *  Created on: 2017年6月26日
+ *      Author: tbm
+ */
+
+#ifndef INCLUDE_LASERSCAN_TO_POINTCLOUD2_LASERSCAN_TO_POINTCLOUD2_NODE_H_
+#define INCLUDE_LASERSCAN_TO_POINTCLOUD2_LASERSCAN_TO_POINTCLOUD2_NODE_H_
+
+
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <laser_geometry/laser_geometry.h>
+
+
+#endif /* INCLUDE_LASERSCAN_TO_POINTCLOUD2_LASERSCAN_TO_POINTCLOUD2_NODE_H_ */
--- src/laserscan_to_pointcloud2_node.cpp
+++ src/laserscan_to_pointcloud2_node.cpp
@@ -1,11 +1,12 @@
 /*
- * laserscan_to_pointcloud2.cpp
+ * laserscan_to_pointcloud2_node.cpp
  *
- *  Created on: 2017年6月8日
+ *  Created on: 2017年6月26日
  *      Author: tbm
  */
 
-#include "../include/laserscan_to_pointcloud2/laserscan_to_pointcloud2.h"
+
+#include "../include/laserscan_to_pointcloud2/laserscan_to_pointcloud2_node.h"
 #include <iostream>
 #include <string>
 #include <boost/bind.hpp>
@@ -20,7 +21,7 @@
 
 using namespace boost;
 
-
+std::string name_space;
 
 void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in, const ros::Publisher& pub )
 {
@@ -82,13 +83,16 @@ void scanCallback2(const sensor_msgs::LaserScan::ConstPtr& scan_in, const ros::P
 	if(count % 200 == 0 )
 	{
 		cloud_ptr_10->is_dense = is_dense;
-		if(!listener_2_.waitForTransform("/world", "party_1/instance_1/erlecopter_2/base_link_inertia", cloud_ptr_10->header.stamp, ros::Duration(1.0)))
+		//at line 130 in /tmp/binarydeb/ros-indigo-tf2-0.5.15/src/buffer_core.cpp
+		//Warning: Invalid argument "/party_1/instance_1/erlecopter_2/base_link_inertia" passed to canTransform argument source_frame in tf2 frame_ids
+		//cannot start with a '/' like:
+		if(!listener_2_.waitForTransform("/world", name_space+"/base_link_inertia" , cloud_ptr_10->header.stamp, ros::Duration(1.0)))
 		{
 			ROS_INFO("cloud_ptr_10->header.frame_id: %s", (cloud_ptr_10->header.frame_id).c_str());
 			ROS_INFO("no transform to base_link_inertia");
 			return ;
 		}
-		pcl_ros::transformPointCloud("party_1/instance_1/erlecopter_2/base_link_inertia", (*cloud_ptr_10), (*cloud_ptr_10_p), listener_2_);
+		pcl_ros::transformPointCloud(name_space+"/base_link_inertia", (*cloud_ptr_10), (*cloud_ptr_10_p), listener_2_);
 		//pub.publish(cloud_ptr_10);
 		pub.publish(cloud_ptr_10_p);
 		//ROS_INFO("cloud_ptr_10 header.frame_id = %s", (cloud_ptr_10->header.frame_id).c_str());
@@ -106,8 +110,11 @@ int main(int argc, char** argv)
 {
 
 	std::cout << "hello world !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-	ros::init(argc, argv, "dddddd");
+	ros::init(argc, argv, "laserscan_to_pointcloud2_node");
+	//use the node in roslaunch file
 	ros::NodeHandle nh;
+	name_space = nh.getNamespace();
+	name_space = name_space.erase(0,1);
 	ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("assembled_cloud_sub_topic", 500);
 	ros::Publisher pub2 = nh.advertise<sensor_msgs::PointCloud2>("assembled_cloud_sub_topic_2", 500);
 	//ros::Publisher pub3 = nh.advertise<sensor_msgs::LaserScan>("assembled_laser_scan", 500);
@@ -117,5 +124,8 @@ int main(int argc, char** argv)
 	ros::spin();
 	return 0;
 }
+
+
+
 
 
